8de260ebb94498093f3eb20107a515414dc045da
Serdar Ergün <generalgord@gmail.com>
2022-11-21 12:31:58 +0300

    Add project files.

---
+++ BestPractices.Api/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestPractices.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ContactController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return configuration["TestMe"].ToString();
+        }
+    }
+}
---
+++ BestPractices.Api/Program.cs
@@ -0,0 +1,52 @@
+namespace BestPractices.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers();
+            builder.Services.AddHealthChecks();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHealthChecks("/api/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    await context.Response.WriteAsync("OK");
+                }
+            });
+
+            app.UseHttpsRedirection();
+
+            app.MapControllers();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
